9ee2760e800515e221a04c847ab7b93f42e88663
Attila Domokos <adomokos@gmail.com>
2014-01-11 15:31:02 -0600

    Recording the first moves

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -4,12 +4,17 @@
 (def board
   [[:_ :_ :_][:_ :_ :_][:_ :_ :_]])
 
-(defn print-board []
+(defn print-board [board]
   (let [print-board-line
     (fn [line]
       (apply str (map #(str (name %) " ") line)))]
   (str "\n"
     (apply str (map #(str (print-board-line %) "\n") board)))))
+
+(defn record-player-move [step board]
+  (let [x (- (first step) 1)
+        y (- (last step) 1)]
+    (-> board (assoc-in [x y] :X))))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -7,4 +7,11 @@
 
 (deftest print-board-test
   (testing "prints an empty tic-tac-toe board"
-    (is (= "\n_ _ _ \n_ _ _ \n_ _ _ \n" (print-board)))))
+    (is (= "\n_ _ _ \n_ _ _ \n_ _ _ \n" (print-board board)))))
+
+(deftest record-player-move-test
+  (testing "records the player's move"
+    (let [updated-board (record-player-move [1 1] board)]
+      (is (= "\nX _ _ \n_ _ _ \n_ _ _ \n" (print-board updated-board))))
+    (let [updated-board (record-player-move [1 3] board)]
+      (is (= "\n_ _ X \n_ _ _ \n_ _ _ \n" (print-board updated-board))))))
