7c5425664bae5810e327f4726c8b00bc7d0cbd89
Attila Domokos <adomokos@gmail.com>
2014-04-03 23:23:38 -0500

    Using an eval loop for user input

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -16,9 +16,17 @@
         y (- (last step) 1)]
     (-> board (assoc-in [x y] :X))))
 
+(def state (atom :running))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println (print-board board)))
+  (println "You are with X. Where do you want to move?")
+  (while (= @state :running)
+    (let [move (read-line)]
+      (if (pos? (count move))
+        (println (print-board board))
+        (reset! state :stop)))))
+
