89c060419f73d6f4b5f32d0136c7d02d1908f2c7
Attila Domokos <adomokos@gmail.com>
2014-04-05 16:22:46 -0500

    Parsing user inputs into coordinates

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -4,22 +4,29 @@
 (def board
   [[:_ :_ :_][:_ :_ :_][:_ :_ :_]])
 
-(defn record-move [board where player]
-  (if (= where "B2")
-    (assoc-in board [1 1] player)
-    (assoc-in board [0 0] player)))
-
 (defn print-board [board]
   (let [print-board-line
-    (fn [line]
-      (apply str (map #(str (name %) " ") line)))]
+        (fn [line]
+          (apply str (map #(str (name %) " ") line)))]
   (str "\n"
     (apply str (map #(str (print-board-line %) "\n") board)))))
 
-(defn record-player-move [step board]
-  (let [x (- (first step) 1)
-        y (- (last step) 1)]
-    (-> board (assoc-in [x y] :X))))
+(defn parse-step [step]
+  (let [parsed-string (seq step)
+        row (first parsed-string)
+        col (last parsed-string)
+        converted-row (fn []
+                        (cond
+                          (= row \A) 0
+                          (= row \B) 1
+                          :else 2))]
+    [(converted-row) (-> col str Integer/parseInt dec)]))
+
+(defn record-move [board step mark]
+  (let [parsed-step (parse-step step)
+        x (first parsed-step)
+        y (last parsed-step)]
+    (-> board (assoc-in [x y] mark))))
 
 (def state (atom :running))
 
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -9,16 +9,19 @@
   (testing "prints an empty tic-tac-toe board"
     (is (= "\n_ _ _ \n_ _ _ \n_ _ _ \n" (print-board board)))))
 
-(deftest records-a-move
+(deftest converts-steps-into-vector
+  (testing "A1 is [0 0]"
+    (is (= [0 0] (parse-step "A1"))))
+  (testing "A2 is [0 1]"
+    (is (= [0 1] (parse-step "A2"))))
+  (testing "C3 is [2 2]"
+    (is (= [2 2] (parse-step "C3")))))
+
+(deftest records-moves-on-a-board
   (let [game-board (atom board)]
     (testing "for user - A1 marks the top left with `X`"
       (is (= [[:X :_ :_][:_ :_ :_][:_ :_ :_]] (swap! game-board record-move "A1" :X))))
-    (testing "for computer - B2 marks the middle with 'O')`"
-      (is (= [[:X :_ :_][:_ :O :_][:_ :_ :_]] (record-move @game-board "B2" :O))))))
-
-(deftest record-player-move-test
-  (testing "records the player's move"
-    (let [updated-board (record-player-move [1 1] board)]
-      (is (= "\nX _ _ \n_ _ _ \n_ _ _ \n" (print-board updated-board))))
-    (let [updated-board (record-player-move [1 3] board)]
-      (is (= "\n_ _ X \n_ _ _ \n_ _ _ \n" (print-board updated-board))))))
+    (testing "for computer - B2 marks the middle with `O`"
+      (is (= [[:X :_ :_][:_ :O :_][:_ :_ :_]] (swap! game-board record-move "B2" :O))))
+    (testing "for user - C3 marks the bottom right with `X`"
+      (is (= [[:X :_ :_][:_ :O :_][:_ :_ :X]] (swap! game-board record-move "C3" :X))))))
