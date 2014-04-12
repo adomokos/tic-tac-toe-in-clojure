dfa9588a83312e71c4da9f7d6108615f39f167be
Attila Domokos <adomokos@gmail.com>
2014-04-12 16:16:29 -0500

    Adding algorithm to map board entries to winning sets

--- src/tic_tac_toe/win.clj
+++ src/tic_tac_toe/win.clj
@@ -4,12 +4,33 @@
   [[[0 0] [0 1] [0 2]]
    [[1 0] [1 1] [1 2]]
    [[2 0] [2 1] [2 2]]
-   [[0 0] [1 0] [2 0]]
-   [[0 1] [1 1] [2 1]]
-   [[0 2] [1 2] [2 2]]
-   [[0 0] [1 1] [2 2]]
-   [[0 2] [1 1] [2 0]]
+   ;[[0 0] [1 0] [2 0]]
+   ;[[0 1] [1 1] [2 1]]
+   ;[[0 2] [1 2] [2 2]]
+   ;[[0 0] [1 1] [2 2]]
+   ;[[0 2] [1 1] [2 0]]
    ])
+
+(defn converts-board-to-moves [board]
+  (let [first-row (first winning-sets)
+        second-row (second winning-sets)
+        third-row (nth winning-sets 2)
+        finds-user-value (fn [coordinate]
+                              (-> board
+                                 (nth (first coordinate))
+                                 (nth (last coordinate))))]
+    [
+     (into {} (map #(conj [%] (finds-user-value %)) first-row))
+     (into {} (map #(conj [%] (finds-user-value %)) second-row))
+     (into {} (map #(conj [%] (finds-user-value %)) third-row))
+    ]
+    ))
+
+(def moves
+  [{[0 0] :X [0 1] :O [0 2] :_}
+   {[1 0] :X [1 1] :X [1 2] :_}
+   {[2 0] :_ [2 1] :_ [2 2] :_}]
+  )
 
 (defn has-winner? [board]
   {:X [[0 0] [0 1] [0 2]]})
--- test/tic_tac_toe/win_test.clj
+++ test/tic_tac_toe/win_test.clj
@@ -2,7 +2,16 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.win :refer :all]))
 
-(deftest finds-winner-test
-  (testing "easily spots a winner for the first line"
-    (let [board [[:X :X :X] [:O :O :_] [:_ :_ :_]]]
-    (is (= {:X [[0 0] [0 1] [0 2]]} (has-winner? board))))))
+(deftest converts-board-to-moves-test
+  (let [board [[:X :X :X] [:O :O :_] [:_ :_ :_]]]
+    (is (= [{[0 0] :X [0 1] :X [0 2] :X}
+            {[1 0] :O [1 1] :O [1 2] :_}
+            {[2 0] :_ [2 1] :_ [2 2] :_}]
+           (converts-board-to-moves board)))))
+
+;(deftest finds-winner-test
+  ;(testing "spots a winner for the first line"
+    ;(let [board [[:X :X :X] [:O :O :_] [:_ :_ :_]]]
+    ;(is (= {:X [[0 0] [0 1] [0 2]]} (has-winner? board))))
+    ;(let [board [[:O :O :O] [:X :X :_] [:_ :_ :_]]]
+    ;(is (= {:O [[0 0] [0 1] [0 2]]} (has-winner? board))))))
