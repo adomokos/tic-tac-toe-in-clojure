632f25fe8c6a7fc80cc3fdd4533f688a19cf41f8
Attila Domokos <adomokos@gmail.com>
2014-06-08 02:26:08 -0500

    Changing comment

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -28,7 +28,7 @@
             (reset! state :stop)))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "The app runner"
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
